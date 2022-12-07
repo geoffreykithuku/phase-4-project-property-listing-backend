2df9804a643f50e7759491e2310d8f4e25aa12b2
alexgicimo <gakurualexgicimo@gmail.com>
2022-12-07 23:39:45 +0300

    show and create ction added to properties_controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,8 +4,54 @@ class PropertiesController < ApplicationController
     properties = Property.all 
 
     render json: properties ,status: :ok
+   end #END index
+
+
+
+   #GET /properties/:id
+   def show
+   property = get_property
+
+   if property
+
+    render json: property,status: :found
+
+   else
+
+    render json: {error:"Property Not Found"}
+
+   end #END if
+
+   end #END show
+    
+
+   #POST /properties
+   def  create
+
+   property = Property.create!(property_params)
+
+   if property 
+    render json: property ,status: :created
    end
-   #
 
+   end #END create
 
-end
+   private
+   #Get a property with given id
+   def get_property
+
+   a_property = Property.find_by(id: params[:id])
+
+   a_property
+
+   end #END get_property
+  
+
+   #Define allowable parameters for mass assignment
+    def property_params
+
+    params.permit(:owner_id,:location,:description,:price,:image)
+
+    end #END property_params
+
+end #END properties_controller
