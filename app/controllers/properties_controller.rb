6893a5d9115f8b2bdeb84e0961eadb1abe79dbf1
alexgicimo <gakurualexgicimo@gmail.com>
2022-12-07 22:45:22 +0300

    Index action for Properties added

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,3 +1,11 @@
 class PropertiesController < ApplicationController
-   
+    #GET /properties
+   def index 
+    properties = Property.all 
+
+    render json: properties ,status: :ok
+   end
+   #
+
+
 end
