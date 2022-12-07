f37c227233e5cb1f654a2b519c1daa1a69f08b71
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-12-08 00:29:12 +0300

    crud action on owner

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,62 @@
 class OwnersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
+
+    #GET /owners
+    def index
+   owners = Owner.all
+   render json: owners ,status: :ok
+   end 
+
+    #GET /owners/:id
+    def show
+
+    owner = find_owner
+    render json: owner,status: :found
+
+    #POST /owners
+   def create 
+    owner =Owner.create!(owner_params)
+    render json: owner ,status: :created
+
+    #PATCH /owners/:id
+    def update
+    owner =find_owner
+    owner.update!(owner_params)
+    render json: owner,status: :accepted
+    end 
+
+   #DELETE owners/:id
+   def destroy
+    owner =find_owner
+    owner.destroy
+    head :no_content
+   end
+
+
+   private
+
+    def owner_params
+       params.permit(:email,:username)
+    end 
+
+   def render_unprocessable_entity_response(invalid)
+
+    render json: {errors: invalid.record.errors.full_messages},status: :unprocessable_entity 
+
+   end 
+
+  def find_owner
+
+  an_owner = Owner.find_by(id: params[:id])
+
+    an_owner 
+  end 
+
+def record_not_found_method
+    render json: {error: "Restaurant not found"}, status: :not_found
+end
+
+end 
 end
