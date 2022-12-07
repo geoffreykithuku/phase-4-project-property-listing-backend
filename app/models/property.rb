4b83a681b4c094148cf298831d78e8de325edfb2
alexgicimo <gakurualexgicimo@gmail.com>
2022-12-08 00:27:28 +0300

    CRUD done

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,5 +3,5 @@ class Property < ApplicationRecord
   has_many :reviews
   has_many :users,through: :reviews
 
-  validates :description,:location,:price,:owner_id ,presence: true
+  validates :description,:location,:price,:owner_id,:image ,presence: true
 end
