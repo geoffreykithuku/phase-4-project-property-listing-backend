99cb3b2828c508dc3e07065e4be7efe44b6797d7
alexgicimo <gakurualexgicimo@gmail.com>
2022-12-07 05:32:00 +0000

    GET /owners/:id  added to owners controller

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,4 +6,17 @@ class OwnersController < ApplicationController
    render json: owners ,status: :ok
 
     end
+
+#GET /owners/:id
+    def show
+
+    owner = Owner.find_by(id: params[:id])
+
+    if owner
+        render json: owner,status: :found
+    else
+        render json: {error:"Owner not found"} ,status: :not_found
+    end
+
+    end
 end
