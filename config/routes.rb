1b28dc6bf909ee843481d65af0df3346b18faaca
alexgicimo <gakurualexgicimo@gmail.com>
2022-12-08 00:23:06 +0300

    Modified allowed actions for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :owners
-  resources :reviews
+  resources :reviews ,except: [:update,:destroy]
   resources :properties
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
