b39b243131664ed4484aa6766a5c0a7ad86ea229
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2022-12-09 02:16:37 +0300

    fix: fixed show route for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,14 @@ class SessionsController < ApplicationController
                 render json: {errors: owner.errors.full_messages}, status: :unauthorized
             end
       end 
+      def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
 
         def destroy
 
